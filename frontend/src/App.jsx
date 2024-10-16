d59fa8dc8fde76e20a007bc299852be71300de35
Arisha Sattar <arishasattar229@gmail.com>
2024-10-17 03:23:28 +0500

    responsive navbar & header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,13 @@
 import './App.css';
-import './assets/css/app.css';
-// import WebHome from "./assets/components/frontend/WebHome";
-// import Home from "./assets/components/admin/Home";
-// import LoginForm from "./assets/components/Auth/LoginForm";
-// import SignupForm from "./assets/components/Auth/SignupForm";
-// import ForgotPassword from "./assets/components/Auth/ForgotPassword";
-// import ResetPassword from './assets/components/Auth/ResetPassword';
-import NotFound from './assets/components/frontend/NotFound';
+// import './assets/css/app.css';
+import WebHome from "./assets/components/frontend/WebHome";
+
+
 
 const App = () => {
   return (
     <>
-      {/* <WebHome /> */}
-      {/* <Home/> */}
-      {/* <LoginForm /> */}
-      {/* <SignupForm /> */}
-      {/* <ForgotPassword/> */}
-      {/* <ResetPassword/> */}
-      <NotFound/>
+      <WebHome />
     </>
   );
 };
---
+++ frontend/src/assets/components/frontend/header/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <p className="text-center py-2">Sign up and get 20% off to your first order.</p>
+    </header>
+  );
+};
+
+export default Header;
