c211bf178b706fa0d522ea9099167446d47a09d9
Arisha Sattar <arishasattar229@gmail.com>
2024-10-16 14:18:56 +0500

    error-404

--- frontend/src/assets/components/frontend/NotFound.jsx
+++ frontend/src/assets/components/frontend/NotFound.jsx
@@ -1,4 +1,5 @@
 import error from '../../Images/404-error.gif';
+import { LuHome } from "react-icons/lu";
 
 const NotFound = () => {
   return (
@@ -8,31 +9,33 @@ const NotFound = () => {
           <div className="row">
             <div className="col-lg-5 col-xl-5 mx-auto">
               <div className="card-body p-4">
-                <h1 className="display-1">
+                <h1 className="display-1 text-center">
                   <span className="text-primary">4</span>
                   <span className="text-danger">0</span>
                   <span className="text-success">4</span>
                 </h1>
-                <h2 className="font-weight-bold display-4">Lost in Space</h2>
-                <p>
+                <h2 className="font-weight-bold display-4 text-center">Lost in Space</h2>
+                <p className='text-center'>
                   You have reached the edge of the universe. The page you
                   requested could not be found. Dont'worry and return to the
                   previous page.
                 </p>
-                <div className="mt-5">
-                  {" "}
+                <div className="mt-5 text-center">
                   <a
                     href="javascript:;"
-                    className="btn btn-primary btn-lg px-md-5 radius-30"
+                    className="btn btn-primary btn-lg px-md-5 d-flex align-items-center justify-content-center gap-2 w-100"
+                    id="custom-bg-btn"
                   >
-                    Go Home
+                    <LuHome />
+                    <span>Go Home</span>
                   </a>
-                  <a
+                  {/* <a
                     href="javascript:;"
-                    className="btn btn-outline-dark btn-lg ms-3 px-md-5 radius-30"
+                    className="btn btn-outline-dark btn-lg ms-3 px-md-5 "
+                    id="custom-btn"
                   >
                     Back
-                  </a>
+                  </a> */}
                 </div>
               </div>
             </div>
